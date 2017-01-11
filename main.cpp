b23b3d7646ccf0b7d07b5861159b1c9f50791009
Walter Oggioni <woggioni@comelz.com>
2017-01-11 18:56:58 +0100

    added QWindowsIntegrationPlugin for Windows build with static Qt

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include "mainwindow.h"
 #include <QApplication>
 
+#if defined(QT_STATIC) && defined(WIN32)
+# include <QtPlugin>
+Q_IMPORT_PLUGIN(QWindowsIntegrationPlugin)
+#endif
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
